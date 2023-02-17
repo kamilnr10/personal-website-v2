81056ee9db0533ee3c8aef265f1dc9512b675c64
Kamil Nowak <kamilnowak@macbook-pro-kamil.home>
2023-02-17 12:53:35 +0100

    fix: add variables for css classes

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,6 +12,21 @@ import arrowDown from "react-useanimations/lib/arrowDown";
 import face from "../../assets/face1.png";
 import BackgroundCircles from "../BackgroundCircles/BackgroundCircles";
 
+const styles = {
+  wrapperHero: "mt-10",
+  nameSpan: "text-secondaryText",
+  wordsSpan: "text-secondaryText font-bold",
+  jobName: "text-xs font-light tracking-[.75em] uppercase",
+  imageWrapper: "relative w-full flex justify-center mt-10",
+  splashImg: "w-96 h-96",
+  avatarImg: "absolute w-64 animate-wiggle",
+  arrowDown: "h-14 flex justify-center items-center",
+  aboutTitle: "mt-10 text-center font-light tracking-[.75em]",
+  circlesWrapper: "w-full h-96 mx-auto rounded-full",
+  iconsWrapper: "flex justify-center my-2",
+  aboutText: "text-sm",
+};
+
 const Hero = () => {
   const [text, count] = useTypewriter({
     words: [
@@ -32,7 +47,7 @@ const Hero = () => {
   };
 
   return (
-    <div className="mt-10">
+    <div className={styles.wrapperHero}>
       <motion.div
         initial={{
           y: -500,
@@ -45,21 +60,19 @@ const Hero = () => {
         }}
       >
         <span>
-          Hello! My name is <span className="text-secondaryText">Kamil</span>.
+          Hello! My name is <span className={styles.nameSpan}>Kamil</span>.
         </span>
         <h1>
-          I'm a <span className="text-secondaryText font-bold">{text}</span>
+          I'm a <span className={styles.wordsSpan}>{text}</span>
           <Cursor />
         </h1>
-        <h3 className="text-xs font-light tracking-[.75em] uppercase">
-          Frontend Developer
-        </h3>
+        <h3 className={styles.jobName}>Frontend Developer</h3>
       </motion.div>
-      <motion.div className="relative w-full flex justify-center mt-10">
+      <motion.div className={styles.imageWrapper}>
         <motion.img
           src={splash}
           alt="splash"
-          className="w-96 h-96"
+          className={styles.splashImg}
           initial={{ scale: 0 }}
           animate={{ scale: 1, rotate: 360 }}
           transition={{ duration: 1, delay: 0.5 }}
@@ -67,25 +80,25 @@ const Hero = () => {
         <motion.img
           src={hero}
           alt="hero"
-          className="absolute w-64 animate-wiggle"
+          className={styles.avatarImg}
           initial={{ scale: 0, opacity: 0 }}
           animate={{ scale: 1, opacity: 1 }}
           transition={{ duration: 0.5 }}
         />
       </motion.div>
       <div
-        className="h-14 flex justify-center items-center"
+        className={styles.arrowDown}
         onClick={() => scrollDown(aboutSection)}
       >
         <UseAnimations animation={arrowDown} size={30} strokeColor="#00df9a" />
       </div>
       <div className="" ref={aboutSection}>
-        <h2 className="mt-10 text-center font-light tracking-[.75em]">ABOUT</h2>
-        <div className="w-full h-96 mx-auto rounded-full">
+        <h2 className={styles.aboutTitle}>ABOUT</h2>
+        <div className={styles.circlesWrapper}>
           <BackgroundCircles />
         </div>
         <div className="">
-          <div className="flex justify-center my-2">
+          <div className={styles.iconsWrapper}>
             <UseAnimations
               animation={github}
               size={40}
@@ -100,7 +113,7 @@ const Hero = () => {
               autoplay={true}
             />
           </div>
-          <p className="text-sm">
+          <p className={styles.aboutText}>
             I have got just over a seven months of experience as a software
             engineer. In this short period of time I've already contributed to
             over a dozen issues and assisted with one escalation project for one
